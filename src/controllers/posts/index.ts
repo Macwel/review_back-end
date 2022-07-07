3e949400cd6b85aebf83157000741af5589ba08c
Macwel <wifi-215@yandex.ru>
2022-07-07 19:40:12 +0500

    add getPostOfUser [Post]

--- src/controllers/posts/index.ts
+++ src/controllers/posts/index.ts
@@ -39,4 +39,16 @@ export default class PostController {
       });
     }
   };
+
+  getPostOfUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const response = await this.#PostService.getPostOfUser(String(req.query.id), Number(req.query.page) || 0);
+      res.status(200).json({ ...response, message: 'Success!' });
+    } catch (error) {
+      res.status(error.status || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  };
 }
